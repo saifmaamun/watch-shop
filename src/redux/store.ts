3b496e493107f74395e62c06e8f8622c2ae0efe5
Saif Maamun <sadifmamun@gmail.com>
2024-01-20 21:39:24 +0600

    RTK Query

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,15 @@
 import {configureStore} from "@reduxjs/toolkit"
 import cartReducer from "./features/cart/cartSlice"
 import productReducer from "./features/products/productSlice"
+import {api} from "./api/apiSlice";
 const store =configureStore({
     reducer:{
         cart:cartReducer,
         product:productReducer,
-    }
+        [api.reducerPath]:api.reducer,
+    },
+     middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware),
 })
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
